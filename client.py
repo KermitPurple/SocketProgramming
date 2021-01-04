f4e3d0c6e846febe0b981165fcac6353a3ecc1cc
Shane McDonough <shanemcdman@gmail.com>
2021-01-03 20:21:11 -0800

    Create logic for client.py

--- client.py
+++ client.py
@@ -1,2 +1,30 @@
 import socket
 import threading
+from server import SocketServer
+
+class SocketClient(SocketServer):
+    """A socket client to connect to a socket server"""
+
+    def connect(self):
+        """Connect to the server"""
+        self.socket.connect(self.addr)
+
+    def disconnect(self):
+        """Disconnect from the server"""
+        self.send(self.disconnect_code)
+
+    def send(self, msg):
+        """Send a message to the server"""
+        msg = msg.encode(self.encode_format)
+        msg_length = len(msg)
+        msg_length = str(msg_length).encode(self.encode_format)
+        msg_length += b' ' * (self.header - len(msg_length))
+        self.socket.send(msg_length)
+        self.socket.send(msg)
+
+if __name__ == "__main__": # driver code
+    client = SocketClient()
+    client.connect()
+    client.send('This works!')
+    client.send(input('Enter a message to send> '))
+    client.disconnect()
--- server.py
+++ server.py
@@ -4,25 +4,34 @@ import threading
 class SocketServer:
     """A socket server to host socket clients"""
 
-    def __init__(self, port = 5050, host = None):
+    def __init__(self, port = 5050, host = None, header = 64, encode_format = 'utf-8', disconnect_code = '!!DISCONNECT!!'):
         self.port = port
-        self.host = socket.gethostbyname(socket.gethostname()) if host is None else host
+        self.host = host if host else socket.gethostbyname(socket.gethostname())
+        self.header = header
+        self.encode_format = encode_format
+        self.disconnect_code = disconnect_code
         self.addr = (self.host, self.port)
-        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server.bind(self.addr)
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     def handle_client(self, conn, addr):
         """Handle a new client joining the server"""
         print(f'[[NEW CONNECTION]] {addr} connected')
-        connected = True
-        while connected:
-            msg = conn.recv()
+        while 1:
+            msg_length = conn.recv(self.header).decode(self.encode_format)
+            if msg_length:
+                msg_length = int(msg_length)
+                msg = conn.recv(msg_length).decode(self.encode_format)
+                if msg == self.disconnect_code:
+                    break
+                print(f'\t[[{addr}]] {msg}')
 
     def start(self):
-        """Star running the server"""
-        self.server.listen()
+        """Start running the server"""
+        self.socket.bind(self.addr)
+        self.socket.listen()
+        print(f'[[LISTENING]] server is listening on {self.host}')
         while 1:
-            conn, addr = self.server.accept()
+            conn, addr = self.socket.accept()
             threading.Thread(target = self.handle_client, args = (conn, addr)).start()
             print(f'[[Active connections]] {threading.activeCount() - 1}')
 
